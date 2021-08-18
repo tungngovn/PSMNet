1c32b31c8baef56784502c203501bcff5d8dceea
tungngovn <tung.ngothanh.hust@gmail.com>
2021-08-18 22:24:18 +0700

    Add batch size

--- scripts/finetune_apolloscape.sh
+++ scripts/finetune_apolloscape.sh
@@ -5,6 +5,7 @@ python finetune.py --maxdisp 192 \
                    --datatype apolloscape \
                    --datapath dataset/apolloscape/stereo_train/ \
                    --epochs 100 \
+                   --batch_size 4 \
                    --loadmodel ./trained/pretrained_sceneflow.tar \
                    --savemodel ./trained/
 
